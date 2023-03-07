a11731e321258d45716d055f0b02943fc78246dd
brince0304 <brince0304@gmail.com>
2023-03-07 14:02:58 +0900

    체크박스 커스텀 훅 생성

---
+++ src/hooks/useCheckbox.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+interface Options {
+    initialValue?: boolean;
+    type?: 'boolean';
+    label: string;
+    icon?: React.ReactNode;
+    checkedIcon?: React.ReactNode;
+}
+
+type returnType = [boolean, (e: React.ChangeEvent<HTMLInputElement>) => void,string];
+
+export function useCheckbox(options?: Options): returnType {
+    const [value, setValue] = React.useState<boolean>(options?.initialValue || false);
+
+    const onChange = React.useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+        const {checked} = e.target;
+        setValue(checked);
+    }, []);
+
+    return [value, onChange, options?.label || ''];
+}
