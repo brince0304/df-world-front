ef21297bb8c6fa48749d96ab27bed1e3c7ec7d62
brince0304 <brince0304@gmail.com>
2023-07-24 22:10:18 +0900

    23-07-24 -REFACTOR : Portal 구현

---
+++ src/utils/Portal.tsx
@@ -0,0 +1,9 @@
+import { useEffect } from "react";
+import ReactDOM from "react-dom";
+
+const ModalPortal = (props:{children: React.ReactNode}) => {
+    const el = document.getElementById('modal') as HTMLElement;
+    return ReactDOM.createPortal(props.children, el);
+}
+
+export default ModalPortal;
