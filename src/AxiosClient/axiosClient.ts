478d4e4451344e4e2b7e087af9ee99f8c26f080d
brince0304 <brince0304@gmail.com>
2023-07-27 17:25:10 +0900

    refactor: AxiosClient 옵션 받을수 있도록 변경

--- src/AxiosClient/axiosClient.ts
+++ src/AxiosClient/axiosClient.ts
@@ -1,10 +1,10 @@
 import { AxiosInstance } from 'axios';
 
 export interface IAxiosClient {
-  get: (url: string) => Promise<any>;
-  post: (url: string, data: any) => Promise<any>;
-  put: (url: string, data: any) => Promise<any>;
-  delete: (url: string) => Promise<any>;
+  get: (url: string,options?:any) => Promise<any>;
+  post: (url: string, data?: any,options?:any) => Promise<any>;
+  put: (url: string, data?: any,options?:any) => Promise<any>;
+  delete: (url: string,options?:any) => Promise<any>;
 }
 
 export class AxiosClient implements IAxiosClient {
@@ -14,36 +14,42 @@ export class AxiosClient implements IAxiosClient {
     this.instance = axiosInstance;
   }
 
-  async get(url: string) {
+  async get(url: string,options?:any) {
     try {
-      const { data } = await this.instance.get(url);
+      const { data } = await this.instance.get(url,options);
       return data;
     } catch (e) {
       throw new Error(e as string);
     }
   }
 
-  async post(url: string, request: any) {
+  async post(url: string, request?: any,options?:any) {
     try {
-      const { data } = await this.instance.post(url, request);
+      const { data } = await this.instance.post(url, request, {
+        ...options
+      });
       return data;
     } catch (e) {
       throw new Error(e as string);
     }
   }
 
-  async put(url: string, request: any) {
+  async put(url: string, request?: any,options?:any) {
     try {
-      const { data } = await this.instance.put(url, request);
+      const { data } = await this.instance.put(url, request,{
+        ...options
+      });
       return data;
     } catch (e) {
       throw new Error(e as string);
     }
   }
 
-  async delete(url: string) {
+  async delete(url: string,options?:any) {
     try {
-      return await this.instance.delete(url);
+      return await this.instance.delete(url,{
+        ...options
+      });
     } catch (e) {
       throw new Error(e as string);
     }
