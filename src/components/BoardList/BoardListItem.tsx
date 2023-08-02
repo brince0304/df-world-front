018609c3464f9c2ac2305c646425cc76fe893901
brince0304 <brince0304@gmail.com>
2023-08-02 21:00:00 +0900

    refactor: BoardList 분리

--- src/components/BoardList/BoardListItem.tsx
+++ src/components/BoardList/BoardListItem.tsx
@@ -1,53 +1,20 @@
-import { Chip, Link, ListItem } from '@mui/material';
-import { getBoardType } from '../../utils/boardUtil';
+import { ListItem } from '@mui/material';
 import Box from '@mui/material/Box';
 import React from 'react';
-import { CharacterChip } from '../../pages/Board';
 import { BoardContent } from '../../interfaces/IBoardList';
 import styled from '@emotion/styled';
-import { useNavigate } from 'react-router-dom';
-import { TagChip } from '../Chips/TagChip';
 import BoardUserAvatar from '../BoardUserAvatar/BoardUserAvatar';
 import BoardCommentView from './BoardCommentView';
+import BoardListItemTitleContent from './BoardListItemContent';
+import BoardChips from './BoardChips';
 
 const BoardListItem = (data: BoardContent) => {
-  const navigate = useNavigate();
-  const handleTypeTagClick = () => {
-    navigate(`/boards?boardType=${data.boardType}`);
-  };
-  const handleListItemClick = (event: React.MouseEvent<HTMLElement>) => {
-    event.preventDefault();
-    event.stopPropagation();
-    navigate(`/boars/${data.id}`);
-  };
   return (
     <ListItem sx={{ width: '100%', border: '0.2px solid #e0e0e0' }}>
       <BoardContainer>
-        <BoardTagContainer>
-          <Chip
-            label={getBoardType(data.boardType)}
-            color="info"
-            clickable={true}
-            sx={{ fontSize: '10px', fontWeight: 'bold' }}
-            size="small"
-            data-type={data.boardType}
-            onClick={handleTypeTagClick}
-          />
-          {data.hashtags.map((tag, index) => (
-            <TagChip key={index} boardType={data.boardType} tag={tag} />
-          ))}
-          {data.character && (
-            <CharacterChip
-              characterName={data.character.characterName}
-              characterImgUrl={data.character.characterImageUrl}
-              adventureName={data.character.adventureName}
-              serverId={data.character.serverId}
-              characterId={data.character.characterId}
-            />
-          )}
-        </BoardTagContainer>
-        <BoardTitleWrapper onClick={handleListItemClick}>{data.boardTitle}</BoardTitleWrapper>
         <BoardUserAvatar src={data.userProfileImgUrl} nickname={data.userNickname} />
+        <BoardListItemTitleContent boardId={String(data.id)} title={data.boardTitle} content={data.boardContent} />
+        <BoardChips data={data} />
         <BoardCommentContainer>
           <BoardCommentView
             boardLikeCount={data.boardLikeCount}
@@ -69,33 +36,6 @@ const BoardContainer = styled(Box)`
   align-items: flex-start;
   justify-content: center;
   width: 100%;
-`;
-
-const BoardTitleWrapper = styled(Link)`
-  display: inline-block;
-  font-size: 18px;
-  font-weight: 600;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  width: 100%;
-  margin-right: 10px;
-  padding-top: 5px;
-  padding-bottom: 5px;
-  cursor: pointer;
-  && {
-    color: #121212;
-    text-decoration: none;
-  }
-`;
-
-const BoardTagContainer = styled(Box)`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: flex-start;
-  width: 100%;
-  gap: 5px;
 `;
 
 const BoardCommentContainer = styled(Box)`
---
+++ src/components/BoardList/BoardListItemContent.tsx
@@ -0,0 +1,60 @@
+import { Box, Link, Typography, styled } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import React from 'react';
+
+const BoardListItemTitleContent = ({ title, content, boardId }: IBoardListItemContentProps) => {
+  const newContent = content.length > 100 ? content.slice(0, 100) + '...' : content;
+  const navigate = useNavigate();
+  const handleListItemClick = (event: React.MouseEvent<HTMLElement>) => {
+    event.preventDefault();
+    event.stopPropagation();
+    navigate(`/boards/${boardId}`);
+  };
+  return (
+    <Container component="div" onClick={handleListItemClick}>
+      <BoardTitleWrapper>
+        <Typography component="h3" style={{ fontSize: '19px', fontWeight: '700' }}>
+          {title}
+        </Typography>
+      </BoardTitleWrapper>
+      <Typography component={'p'} style={{ fontSize: '14px', color: 'gray' }}>
+        {newContent}
+      </Typography>
+    </Container>
+  );
+};
+
+export default BoardListItemTitleContent;
+
+interface IBoardListItemContentProps {
+  content: string;
+  title: string;
+  boardId: string;
+}
+
+const Container = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  width: 100%;
+  cursor: pointer;
+`;
+
+const BoardTitleWrapper = styled(Link)`
+  display: inline-block;
+  white-space: nowrap;
+  overflow: hidden;
+  text-align: left;
+  text-overflow: ellipsis;
+  width: 100%;
+  margin-right: 10px;
+  padding-top: 5px;
+  && {
+    color: #121212;
+    text-decoration: none;
+  }
+  &:hover {
+    text-decoration: underline;
+  }
+`;
