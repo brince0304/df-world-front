30ae34a158491de7f5202d1bee6425fba745bd82
brince0304 <brince0304@gmail.com>
2023-08-09 03:34:29 +0900

    fix: useChangeUserNickname 수정

--- src/hooks/myPageHooks/mutations/useChangeUserNicknameMuation.ts
+++ src/hooks/myPageHooks/mutations/useChangeUserNicknameMuation.ts
@@ -1,17 +1,19 @@
 import { useMyPageService } from '../../../context/myPageServiceContext';
 import useMyPageSuccess from '../useMyPageSuccess';
 import useMyPageError from '../useMyPageError';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { QUERY_KEY } from '../../../constants/myConstants';
 
 const useChangeUserNicknameMuation = () => {
   const { changeUserNickname } = useMyPageService();
   const { handleChangeUserNicknameSuccess } = useMyPageSuccess();
   const { handleChangeUserNicknameError } = useMyPageError();
-
+  const queryClient = useQueryClient();
   const { mutate: handleChangeUserNickname } = useMutation({
     mutationFn: changeUserNickname,
     onSuccess: () => {
       handleChangeUserNicknameSuccess();
+      queryClient.invalidateQueries([QUERY_KEY.user]);
     },
     onError: () => {
       handleChangeUserNicknameError();
